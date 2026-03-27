fe7c0689b97351a5948a4ed061f316bd4192966f
kaxaniku <kaxaniku@gmail.com>
2026-03-27 18:45:55 +0400

    Add domain models and value objects for web store.

---
+++ WebStore.Domain/Entities/Category.cs
@@ -0,0 +1,36 @@
+﻿using WebStore.Domain.ValueObjects;
+
+namespace WebStore.Domain;
+public sealed class Category
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+
+    private Category()
+    {
+    }
+
+    public static Category Create(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Name name cannot be null or empty.", nameof(name));
+        return new Category
+        {
+            Name = name,
+        };
+    }
+
+    public static void SetId(Category category, int id)
+    {
+        if (id <= 0)
+            throw new ArgumentException("Id must be a positive integer.", nameof(id));
+        category.Id = id;
+    }
+
+    public static void UpdateName(Category category, string newName)
+    {
+        if (string.IsNullOrWhiteSpace(newName))
+            throw new ArgumentException("Name cannot be null or empty.", nameof(newName));
+        category.Name = newName;
+    }
+}
---
+++ WebStore.Domain/Entities/Order.cs
@@ -0,0 +1,34 @@
+﻿namespace WebStore.Domain;
+public sealed class Order
+{
+    public int Id { get; private set; }
+    public decimal TotalPrice { get; private set; }
+
+    private Order()
+    {
+    }
+
+    public static Order Create(decimal totalPrice)
+    {
+        if (totalPrice < 0)
+            throw new ArgumentException("Total price cannot be negative.", nameof(totalPrice));
+        return new Order
+        {
+            TotalPrice = totalPrice,
+        };
+    }
+
+    public static void SetId(Order order, int id)
+    {
+        if (id <= 0)
+            throw new ArgumentException("Id must be a positive integer.", nameof(id));
+        order.Id = id;
+    }
+
+    public static void UpdateTotalPrice(Order order, decimal newPrice)
+    {
+        if (newPrice < 0)
+            throw new ArgumentException("Total price cannot be negative.", nameof(newPrice));
+        order.TotalPrice = newPrice;
+    }
+}
